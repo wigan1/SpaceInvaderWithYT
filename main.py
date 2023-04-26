89f75596f05f254c7d88265c413ee834a54a6563
wigan1 <wdgancarczyk@gmail.com>
2023-04-26 21:22:00 +0200

    Pygame Tutorial - 3 - Changing the Title, Logo and Background color

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Background
+    screen.fill((100, 20, 20))
+    pygame.display.update()
