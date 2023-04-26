ecd56bccdb352714017dc767a3184a5b85507bb6
wigan1 <wdgancarczyk@gmail.com>
2023-04-26 23:16:54 +0200

    Pygame Tutorial - 6 - Keyboard Input Controls/ Key Pressed Event

--- main.py
+++ main.py
@@ -15,20 +15,31 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("player.png")
 playerX = 368
 playerY = 480
+playerX_change = 0
 
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
 
 # Game loop
 running = True
 while running:
     # Background
     screen.fill((100, 20, 20))
-    playerY -= 0.1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # if keystroke is pressed check whether its right of left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
